17fef1d68ab6c556812af6512f98b99770c6d524
Mérill T <merill.teterel@epitech.eu>
2020-01-07 10:49:45 +0100

    Added login & register

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path={"/"} component={Home} />
+        <Route exact path={"/login"} component={Login} />
+        <Route exact path={"/register"} component={Register} />
         <Route render={() => <h1>Not Found</h1>} />
       </Switch>
     </Router>
