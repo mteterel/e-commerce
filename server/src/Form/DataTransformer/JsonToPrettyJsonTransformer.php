096e3a3f4a3815df81e4d0d7ff403c3c2082aaf7
Arnaud <arnaud.delgerie@epitech.eu>
2020-01-17 16:38:46 +0100

    modificationn easy admin

--- server/src/Form/DataTransformer/JsonToPrettyJsonTransformer.php
+++ server/src/Form/DataTransformer/JsonToPrettyJsonTransformer.php
@@ -5,15 +5,16 @@ use Symfony\Component\Form\DataTransformerInterface;
 
 class JsonToPrettyJsonTransformer implements DataTransformerInterface
 {
+
     public function transform($value)
     {
-        if (is_array($value))
-            $value = json_encode($value);
+        if (is_array($value))         
+            $value = json_encode($value);            
         return json_encode(json_decode($value), JSON_PRETTY_PRINT);
     }
 
     public function reverseTransform($value)
-    {
+    {        
         return json_decode($value, TRUE);
     }
 }
--- server/src/Form/Type/JsonType.php
+++ server/src/Form/Type/JsonType.php
@@ -1,21 +1,23 @@
 <?php
 namespace App\Form\Type;
 
-use App\Form\DataTransformer\JsonToPrettyJsonTransformer;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Form\DataTransformer\JsonToPrettyJsonTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class JsonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    {        
         parent::buildForm($builder, $options);
-        $builder->addViewTransformer(new JsonToPrettyJsonTransformer());
+        $builder->addViewTransformer(new JsonToPrettyJsonTransformer());                        
     }
 
     public function getParent()
-    {
+    {   
         return TextareaType::class;
     }
 }
