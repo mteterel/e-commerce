bf7f7a4e812dd97c5d5b60e27b5614252bebe11f
davy <davy.catteeuw@epitech.eu>
2020-01-17 12:59:53 +0100

    navbar update with bootstrap

--- client/src/components/AppNavBar_1.jsx
+++ client/src/components/AppNavBar_1.jsx
@@ -9,43 +9,69 @@ import {
   Navbar,
   OverlayTrigger,
   Popover,
-  PopoverContent
+  PopoverContent,
+  Row,
+  Col
 } from "react-bootstrap";
 import MiniCart from "./MiniCart";
 import { connect } from "react-redux";
+import Logo from "../assets/master.png";
+import { 
+  FaSearch, 
+  FaUser,
+  FaShoppingCart
+ } from "react-icons/fa";
+import styles from "./AppNavBar_1.module.scss";
 
 const AppNavBar1 = props => {
   return (
     <Navbar bg="dark" variant="dark" style={{ padding: "1em" }}>
       <Container>
-        <Navbar.Brand href="/">Omega Gaming</Navbar.Brand>
-        <InputGroup style={{ width: "60%" }}>
-          <FormControl type="text" placeholder="Search for products" />
-          <InputGroup.Append>
-            <Button>Search</Button>
-          </InputGroup.Append>
-        </InputGroup>
-        <Nav>
-          <OverlayTrigger
-            trigger={"click"}
-            placement="bottom"
-            overlay={
-              <Popover id={"popover-mini-cart"}>
-                <PopoverContent>
-                  <MiniCart />
-                </PopoverContent>
-              </Popover>
-            }
-          >
-            <Nav.Link>
-              <span>Cart&nbsp;</span>
-              <Badge variant={"primary"}>{props.cartProducts.length}</Badge>
+        <Row>
+          <Col xs lg="2">
+            <Navbar.Brand href="/"><img src={Logo} className={styles.BaseLogo}/></Navbar.Brand>
+          </Col>
+          <Col xs lg="8">
+            <InputGroup className={styles.search_bar}>
+              <FormControl className={styles.search} type="search" placeholder="Search for products"/>
+              <InputGroup.Append>
+                <Button className={styles.SearchButton}><FaSearch className={styles.FaSearch}/></Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Col>
+          <Col xs lg="1">
+          {/* <Nav> */}
+          {/* <div className={styles.test}> */}
+            <OverlayTrigger
+              trigger={"click"}
+              placement="bottom"
+              overlay={
+                <Popover id={"popover-mini-cart"}>
+                  <PopoverContent>
+                    <MiniCart />
+                  </PopoverContent>
+                </Popover>
+              }
+            >
+              
+              <Nav.Link>
+                <div className={styles.testdiv}>
+                <FaShoppingCart className={styles.FaShoppingCart} color="white"/>              
+                <Badge pill variant={"primary"} className={styles.BadgeCart}>{props.cartProducts.length}</Badge>
+                </div>
+                <span className={styles.cart}>Shopping&nbsp;</span>
+              </Nav.Link>
+            </OverlayTrigger>
+          {/* </div> */}
+          </Col>
+          <Col xs lg="1">
+            <Nav.Link>  
+              <FaUser className={styles.FaUser} color="white"/>
+              <span className={styles.user}>{props.user.username ?? "Account"}</span>
             </Nav.Link>
-          </OverlayTrigger>
-          <Nav.Link>
-            <span>{props.user.username ?? "Not Logged In"}</span>
-          </Nav.Link>
-        </Nav>
+          {/* </Nav> */}
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
