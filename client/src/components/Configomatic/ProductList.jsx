acb0ae474b9898379fbc931d29d2c21b2cf6129e
Arnaud <arnaud.delgerie@epitech.eu>
2020-01-18 10:46:54 +0100

    add productList component (configomaticView)

---
+++ client/src/components/Configomatic/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = () => {
+    return (
+        <div>
+            Liste des produits
+        </div>
+    );
+};
+
+export default ProductList;
--- client/src/views/Configomatic.jsx
+++ client/src/views/Configomatic.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import CategorieList from "../components/Configomatic/CategorieList";
+import ProductList from "../components/Configomatic/ProductList";
+import { Row, Col } from "react-bootstrap";
 
 const Configomatic = () => {
     return (
         <div>
-            <CategorieList />
+            <Row>
+                <Col md={6}>
+                    <CategorieList />
+                </Col>
+                <Col md={6}>
+                    <ProductList />
+                </Col>
+            </Row>
         </div>
     );
 };
