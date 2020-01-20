cd597720ce371ad8e3d148b6408e2c95cba00e0c
davy <davy.catteeuw@epitech.eu>
2020-01-21 00:09:29 +0100

    Views update

--- client/src/views/_BaseLayout.jsx
+++ client/src/views/_BaseLayout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Card, Container } from "react-bootstrap";
 import AppNavBar1 from "../components/AppNavBar_1";
 import AppNavBar2 from "../components/AppNavBar_2";
+import CarousselPromo from "../components/CarousselPromo";
+
 
 const _BaseLayout = ({ children }) => {
   return (
@@ -10,9 +12,10 @@ const _BaseLayout = ({ children }) => {
         <AppNavBar1 />
         <AppNavBar2 />
       </header>
+      {/* <CarousselPromo></CarousselPromo> */}
       <main style={{ marginTop: "2em", marginBottom: "2em" }}>
         <Container>
-          <Card body>{children}</Card>
+          <body>{children}</body>
         </Container>
       </main>
     </div>
